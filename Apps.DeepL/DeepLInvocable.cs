d02c855fb0285546ca3d15f4ef75a6bd93950327
bZverok <137277669+bZverok@users.noreply.github.com>
2023-12-28 13:04:37 +0200

    New files (#16)
    * added new files
    
    * Update Apps.DeepL.csproj

--- Apps.DeepL/DeepLInvocable.cs
+++ Apps.DeepL/DeepLInvocable.cs
@@ -1,22 +1,15 @@
 ﻿using Apps.DeepL.Factories;
 using Blackbird.Applications.Sdk.Common;
-using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using DeepL;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Apps.DeepL
+namespace Apps.DeepL;
+
+public class DeepLInvocable : BaseInvocable
 {
-    public class DeepLInvocable : BaseInvocable
+    protected Translator Client { get; }
+    public DeepLInvocable(InvocationContext invocationContext) : base(invocationContext) 
     {
-        protected Translator Client { get; }
-        public DeepLInvocable(InvocationContext invocationContext) : base(invocationContext) 
-        {
-            Client = TranslatorFactory.GetTranslator(invocationContext.AuthenticationCredentialsProviders);
-        }
+        Client = TranslatorFactory.GetTranslator(invocationContext.AuthenticationCredentialsProviders);
     }
 }
