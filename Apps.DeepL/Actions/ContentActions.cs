60c3bc5cee9f57917edd8d65476964f6fab104bf
mathijs-bb <mathijs.sonnemans@blackbird.io>
2025-05-16 18:04:52 +0200

    Added experimental content action

---
+++ Apps.DeepL/Actions/ContentActions.cs
@@ -0,0 +1,108 @@
+﻿extern alias XliffContent;
+
+using Apps.DeepL.Requests;
+using Apps.DeepL.Responses;
+using Apps.DeepL.Utils;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Actions;
+using Blackbird.Applications.Sdk.Common.Exceptions;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
+using Blackbird.Xliff.Utils.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XliffContent::Blackbird.Xliff.Utils.Models.Content;
+using XliffContent::Blackbird.Xliff.Utils.Constants;
+using XliffContent::Blackbird.Xliff.Utils.Serializers.Xliff2;
+using XliffContent::Blackbird.Xliff.Utils.Extensions;
+using DeepL;
+using DocumentFormat.OpenXml.Office2016.Excel;
+using Blackbird.Xliff.Utils.Constants;
+using Apps.DeepL.Constants;
+using Apps.DeepL.Requests.Content;
+
+namespace Apps.DeepL.Actions;
+
+[ActionList]
+public class ContentActions(InvocationContext invocationContext, IFileManagementClient fileManagementClient)
+    : DeepLInvocable(invocationContext)
+{
+    [Action("Translate content", Description = "Translate content retrieved from a CMS. The output can be used in compatible actions.")]
+    public async Task<FileResponse> TranslateContent([ActionParameter] ContentTranslationRequest input)
+    {
+        if (string.IsNullOrWhiteSpace(input.TargetLanguage))
+        {
+            throw new PluginMisconfigurationException("The target language can not be empty, please fill the 'Target language' field and make sure it has a valid language code");
+        }
+
+        var supportedLanguages = LanguageConstants.TargetLanguages.Keys;
+        if (!supportedLanguages.Contains(input.TargetLanguage.ToUpperInvariant()))
+        {
+            throw new PluginMisconfigurationException($"The target language '{input.TargetLanguage}' is not supported. Please select a valid language.");
+        }
+
+        var stream = await fileManagementClient.DownloadAsync(input.File);
+        var content = await FileGroup.TryParse(stream);
+        if (content == null) throw new PluginApplicationException("Something went wrong parsing this XLIFF file. Please send a copy of this file to the team for inspection!");
+        content.SourceLanguage ??= input.SourceLanguage;
+        content.TargetLanguage ??= input.TargetLanguage;
+
+        var options = new TextTranslateOptions
+        {
+            PreserveFormatting = true,
+            Formality = GetFormality(input.Formality),
+            GlossaryId = input.GlossaryId,
+            TagHandling = "html",
+            Context = input.Context,
+            ModelType = GetModelType(input.ModelType)
+        };
+
+        var batches = content.IterateSegments().Where(x => !x.Ignorable && x.IsInitial()).Batch(100);
+
+        foreach(var batch in batches)
+        {
+            var results = await ErrorHandler.ExecuteWithErrorHandlingAsync(async () =>
+                await Client.TranslateTextAsync(batch.Select(x => x.GetSource()), content.SourceLanguage, content.TargetLanguage, options));
+
+            var batchAsArray = batch.ToArray();
+            for(int i = 0; i < results.Length; i++)
+            {
+                var segment = batchAsArray[i];
+                var result = results[i];
+
+                segment.SetTarget(result.Text, TagParsing.Html);
+                segment.State = SegmentState.Translated;
+            }
+        }
+
+        var xliffStream = Xliff2Serializer.Serialize(content).ToStream();
+        var fileName = input.File.Name.EndsWith("xliff") || input.File.Name.EndsWith("xlf") ? input.File.Name : input.File.Name + ".xliff";
+        var uploadedFile = await fileManagementClient.UploadAsync(xliffStream, "application/xliff+xml", fileName);
+        return new FileResponse { File = uploadedFile };
+    }
+
+    private static Formality GetFormality(string? formalityString)
+    {
+        return formalityString switch
+        {
+            "more" => Formality.More,
+            "less" => Formality.Less,
+            "prefer_more" => Formality.PreferMore,
+            "prefer_less" => Formality.PreferLess,
+            _ => Formality.Default
+        };
+    }
+
+    private static ModelType GetModelType(string? modelTypeString)
+    {
+        return modelTypeString switch
+        {
+            "quality_optimized" => ModelType.QualityOptimized,
+            "prefer_quality_optimized" => ModelType.PreferQualityOptimized,
+            _ => ModelType.LatencyOptimized
+        };
+    }
+}
---
+++ Tests.DeepL/ContentTests.cs
@@ -0,0 +1,27 @@
+﻿using Apps.DeepL.Actions;
+using Apps.DeepL.Requests;
+using Apps.DeepL.Requests.Content;
+using Blackbird.Applications.Sdk.Common.Files;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.DeepL.Base;
+
+namespace Tests.DeepL;
+
+[TestClass]
+public class ContentTests : TestBase
+{
+    [TestMethod]
+    public async Task Translate_Contentful_html()
+    {
+        var actions = new ContentActions(InvocationContext, FileManager);
+        var file = new FileReference { Name = "contentful.html" };
+        var result = await actions.TranslateContent(new ContentTranslationRequest() { File = file, SourceLanguage = "en", TargetLanguage = "nl" });
+
+        Assert.AreEqual(result.File.Name, "contentful.html.xliff");
+    }
+}
