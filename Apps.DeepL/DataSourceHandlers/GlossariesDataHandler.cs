cf50be4f2ee37ad9dc83ce7fc1c950862facb894
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-12-30 10:44:03 +0200

    remove v2 glossary request from GlossariesDataHandler (causes duplicates, v3 returns the same v2 data + v3)

--- Apps.DeepL/DataSourceHandlers/GlossariesDataHandler.cs
+++ Apps.DeepL/DataSourceHandlers/GlossariesDataHandler.cs
@@ -11,20 +11,12 @@ public class GlossariesDataHandler(InvocationContext invocationContext)
 {
     public async Task<IEnumerable<DataSourceItem>> GetDataAsync(DataSourceContext context, CancellationToken ct)
     {
-        var glossariesv3Request = new RestRequest("https://api.deepl.com/v3/glossaries", Method.Get);
-        var glossariesv3 = await RestClient.ExecuteAsync<ListGlossariesV3Response>(glossariesv3Request).ConfigureAwait(false);
-        if (!glossariesv3.IsSuccessful)
-            throw new PluginApplicationException($"{glossariesv3.StatusCode} – {glossariesv3.Content}");
+        var request = new RestRequest("https://api.deepl.com/v3/glossaries", Method.Get);
+        var response = await RestClient.ExecuteAsync<ListGlossariesV3Response>(request).ConfigureAwait(false);
+        if (!response.IsSuccessful)
+            throw new PluginApplicationException($"{response.StatusCode} – {response.Content}");
 
-        var glossariesv2 = await Client.ListGlossariesAsync(ct);
-
-        var v2Items = glossariesv2.Select(x => new DataSourceItem(x.GlossaryId, x.Name));
-        var v3Items = glossariesv3.Data?.Glossaries.Select(x => new DataSourceItem(x.GlossaryId, x.Name)) ?? [];
-
-        var items = new List<DataSourceItem>();
-        items.AddRange(v2Items);
-        items.AddRange(v3Items);
-
+        var items = response.Data?.Glossaries.Select(x => new DataSourceItem(x.GlossaryId, x.Name)) ?? [];
         if (!string.IsNullOrWhiteSpace(context.SearchString))
             return items.Where(x => x.DisplayName.Contains(context.SearchString, StringComparison.OrdinalIgnoreCase));
 
